502df8f307f5f38231873880463b7b5f5aff1cc2
Trifonko V. <vtrifonko@gmail.com>
2025-05-28 20:10:43 +0300

    Update operations.c

--- Course Project/src/operations.c
+++ Course Project/src/operations.c
@@ -1,4 +1,5 @@
 #include "include.h"
+#include <unistd.h>
 
 void result_operations(char *filename) {
     int highlight = 0;
@@ -8,28 +9,27 @@ void result_operations(char *filename) {
 
     clear();
     attron(COLOR_PAIR(2) | A_BOLD);
-    mvprintw(0, 0, "File operations: %s", filename);
+    mvprintw(0, 0, "Operations: %s", filename);
     attroff(COLOR_PAIR(2) | A_BOLD);
 
     const char *operations[] = {"Open", "Delete", "Copy", "Back"};
     int menu_size = 4;
 
     while (1) {
-        clear();  
+        clear();
         attron(COLOR_PAIR(1) | A_BOLD);
-        mvprintw(0, 0, "File operations: %s", filename);
+        mvprintw(0, 0, "Operations: %s", filename);
         attroff(COLOR_PAIR(1) | A_BOLD);
 
         for (int i = 0; i < menu_size; i++) {
-            if (i == highlight) {
-                attron(COLOR_PAIR(4)); 
-            } else {
-                attron(COLOR_PAIR(5)); 
-            }
+            if (i == highlight)
+                attron(COLOR_PAIR(4));
+            else
+                attron(COLOR_PAIR(5));
 
             mvprintw(2 + i, 0, "%d. %s", i + 1, operations[i]);
-            attroff(COLOR_PAIR(4)); 
-            attroff(COLOR_PAIR(5)); 
+            attroff(COLOR_PAIR(4));
+            attroff(COLOR_PAIR(5));
         }
 
         choice = getch();
@@ -42,41 +42,51 @@ void result_operations(char *filename) {
                 break;
             case 10: 
                 switch (highlight) {
-                    case 0: {  
+                    case 0: { 
                         snprintf(cmd, MAX_CMD, "xdg-open \"%s\"", filename);
                         system(cmd);
-                        strcpy(msg, "File opened successfully.");
+                        strcpy(msg, "Opened successfully.");
                         break;
                     }
-                    case 1: {  
+                    case 1: { 
                         struct stat st;
-                        if (stat(filename, &st) != 0) {
-                            strcpy(msg, "Failed to access file.");
+                        if (lstat(filename, &st) != 0) {
+                            strcpy(msg, "Failed to access.");
                             mvprintw(menu_size + 6, 0, "%s Press any key to continue...", msg);
                             getch();
                             break;
                         }
-                    
-                        if (S_ISDIR(st.st_mode)) {
-                            snprintf(cmd, MAX_CMD, "rm -r \"%s\"", filename);  
+
+                        if (S_ISLNK(st.st_mode)) {
+                            
+                            if (unlink(filename) == 0)
+                                strcpy(msg, "Link deleted successfully.");
+                            else
+                                strcpy(msg, "Failed to delete link.");
+                        } else if (S_ISDIR(st.st_mode)) {
+                            snprintf(cmd, MAX_CMD, "rm -r \"%s\"", filename);
+                            if (system(cmd) == 0)
+                                strcpy(msg, "Directory deleted successfully.");
+                            else
+                                strcpy(msg, "Failed to delete directory.");
                         } else {
-                            snprintf(cmd, MAX_CMD, "rm \"%s\"", filename);    
+                            snprintf(cmd, MAX_CMD, "rm \"%s\"", filename);
+                            if (system(cmd) == 0)
+                                strcpy(msg, "File deleted successfully.");
+                            else
+                                strcpy(msg, "Failed to delete file.");
                         }
-                    
-                        if (system(cmd) == 0) {
-                            strcpy(msg, "File deleted successfully.");
-                            for (int i = 0; i < search_results.result_count; i++) {
-                                if (strcmp(search_results.results[i], filename) == 0) {
-                                    for (int j = i; j < search_results.result_count - 1; j++) {
-                                        strcpy(search_results.results[j], search_results.results[j + 1]);
-                                    }
-                                    search_results.result_count--;
-                                    break;
+
+                        for (int i = 0; i < search_results.result_count; i++) {
+                            if (strcmp(search_results.results[i], filename) == 0) {
+                                for (int j = i; j < search_results.result_count - 1; j++) {
+                                    strcpy(search_results.results[j], search_results.results[j + 1]);
                                 }
+                                search_results.result_count--;
+                                break;
                             }
-                        } else {
-                            strcpy(msg, "Failed to delete file.");
                         }
+
                         break;
                     }
                     case 2: { 
@@ -86,12 +96,26 @@ void result_operations(char *filename) {
                         getnstr(dest, MAX_PATH - 1);
                         noecho();
 
-                        snprintf(cmd, MAX_CMD, "cp -r \"%s\" \"%s\"", filename, dest);
-                        if (system(cmd) == 0) {
-                            strcpy(msg, "File copied successfully.");
-                        } else {
-                            strcpy(msg, "Failed to copy file.");
+                        struct stat st;
+                        if (lstat(filename, &st) != 0) {
+                            strcpy(msg, "Failed to access.");
+                            break;
                         }
+
+                        if (S_ISLNK(st.st_mode)) {
+
+                            snprintf(cmd, MAX_CMD, "cp -P \"%s\" \"%s\"", filename, dest);
+                        } else if (S_ISDIR(st.st_mode)) {
+                            snprintf(cmd, MAX_CMD, "cp -r \"%s\" \"%s\"", filename, dest);
+                        } else {
+                            snprintf(cmd, MAX_CMD, "cp \"%s\" \"%s\"", filename, dest);
+                        }
+
+                        if (system(cmd) == 0)
+                            strcpy(msg, "Copied successfully.");
+                        else
+                            strcpy(msg, "Failed to copy.");
+
                         break;
                     }
                     case 3: 
@@ -103,3 +127,4 @@ void result_operations(char *filename) {
         }
     }
 }
+
